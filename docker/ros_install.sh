71f9a6752a075a6840ebc2e4be058f83d04e4a0b
Yin Wu <yinwu.life@gmail.com>
2021-11-19 19:54:02 +0100

    docker init

---
+++ docker/ros_install.sh
@@ -0,0 +1,20 @@
+ROS_DISTRO="noetic"
+
+# set up sources.list
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# set up keys
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+
+apt-get update
+apt-get install -y ros-$ROS_DISTRO-desktop-full
+
+# environment
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+apt-get install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+# rosdep
+rosdep init
+rosdep update
